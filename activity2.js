7bd70f8c3755740fae99ed60341b655650102e3a
Rohityadav2011 <75617805+Rohityadav2011@users.noreply.github.com>
2021-01-13 18:18:44 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,7 @@
+function back(){
+    window.location="activity_1.html";
+}
+function getscore(){
+   score=localStorage.getItem("score");
+   document.getElementById("update").innerHTML="score:"+score;
+}
